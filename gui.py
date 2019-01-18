d602f26056c1546ffb9e30976aa829b39cd1921f
xletmjm416 <mjm416@imperial.ac.uk>
2019-01-18 11:30:48 +0000

    Changed gui to have simulation in it

--- gui.py
+++ gui.py
@@ -9,41 +9,51 @@ import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import tkinter as tk
+import oslo
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
-        self.plotframe = PlotFrame(self)
-        self.plotframe.pack(side="top")
+        #self.plotframe = PlotFrame(self)
+        #self.plotframe.pack(side="top")
         
-        self.refresh = tk.Button(self, text="Refresh", command=self.plotframe.refreshFigure)
-        self.refresh.pack()
+        self.modelframe = ModelFrame(self)
+        self.modelframe.pack(side="top")
+        
+        #self.refresh = tk.Button(self, text="Refresh", command=self.plotframe.refreshFigure)
+        #self.refresh.pack(side="bottom")
+        
+        self.drive = tk.Button(self, text="Drive", command=self.modelframe.drive)
+        self.drive.pack(side="bottom")
         
         self.quit = tk.Button(self, text="Quit", command=self.master.destroy)
-        self.quit.pack()
+        self.quit.pack(side="bottom")
         
         self.pack()
 
-class PlotFrame(tk.Frame):
-    def __init__(self,master=None):
+class ModelFrame(tk.Frame):
+    def __init__(self, master=None):
         super().__init__(master)
+        L_size = 8
+        self.model = oslo.Oslo(L_size)
         self.fig = plt.Figure(figsize=(6, 6), dpi=100)
         self.subplot = self.fig.add_subplot(111)
-        self.contents, = self.subplot.plot([1,2,3,4,5], [1,2,3,2,1])
+        self.contents, = self.subplot.plot(range(len(self.model.state)), self.model.state)
         self.canvas = FigureCanvasTkAgg(self.fig, self)
         self.canvas.draw()
         self.canvas.get_tk_widget().pack()
         self.pack()
         
-    def refreshFigure(self):
-        x = [1,2,3,4,5,6]
-        y = [1,4,3,2,1,7]
+    def drive(self):
+        x = range(len(self.model.state))
+        y = self.model.state
+        next(self.model)
         self.contents.set_data(x, y)
         ax = self.canvas.figure.axes[0]
-        ax.set_xlim(min(x), max(x))
-        ax.set_ylim(min(y), max(y))        
+        ax.set_xlim(0, len(self.model.state))
+        ax.set_ylim(0, max(y))
         self.canvas.draw()
-
+        
 root = tk.Tk()
 app = Application(master=root)
 app.mainloop()
